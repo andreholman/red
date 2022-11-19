baa8fed6773478653285940fb64bd49dbe400396
Andre Holman <andre@andreholman.com>
2022-11-19 15:12:09 -0800

    Add flairs feature

--- red/models.py
+++ red/models.py
@@ -1,31 +1,52 @@
-from unittest.util import _MAX_LENGTH
-from django.db import models
-from django.core.validators import RegexValidator
-from django.contrib.postgres.fields import ArrayField
 import datetime
+from unittest.util import _MAX_LENGTH
+
+from django.core.validators import RegexValidator
+from django.db import models
 
 # Create your models here.
 
 class Sub(models.Model):
     created = models.DateTimeField(auto_now_add=True, editable=False, null=False, blank=False)
 
-    name = models.CharField(max_length=16, unique=True)
+    name = models.CharField(max_length=16, validators=[RegexValidator(regex=r"^[0-9a-zA-Z]*$", message="Sub name must be alphanumeric.", code="nomatch")], unique=True)
     followers = models.IntegerField(default=0)
-    
+
     def __str__(self):
         return self.name
+
+class PostFlair(models.Model): # Can be applied to posts and users.
+    created = models.DateTimeField(auto_now_add=True, editable=False, null=False, blank=False)
+
+    text = models.CharField(max_length=64)
+    color = models.CharField(max_length=6, validators=[RegexValidator(regex=r"^[a-fA-F0-9]{6}", message='Invalid hex color', code="nomatch")], null=True, blank=True)
+    sub = models.ForeignKey(Sub, on_delete=models.CASCADE, related_name="flairs")
+
+    def __str__(self):
+        return f"{self.text} in {self.sub}"
+
+class UserFlair(models.Model):
+    created = models.DateTimeField(auto_now_add=True, editable=False, null=False, blank=False)
+
+    text = models.CharField(max_length=64)
+    color = models.CharField(max_length=6, validators=[RegexValidator(regex=r"^[a-fA-F0-9]{6}", message='Invalid hex color', code="nomatch")], null=True, blank=True)
+    sub = models.ForeignKey(Sub, on_delete=models.CASCADE, related_name="userflairs")
+
+    def __str__(self):
+        return f"{self.text} in {self.sub}"
 
 class User(models.Model):
     created = models.DateTimeField(auto_now_add=True, editable=False, null=False, blank=False)
 
     username = models.CharField(max_length=16, unique=True)
-    email = models.CharField(max_length=256, validators=[RegexValidator(regex='.+@.+\..{2,}', message='Invalid email', code="nomatch")], unique=True)
-    password = models.CharField(max_length=64, validators=[RegexValidator(regex='^.{64}$', message='256-bit expected', code='nomatch')])
+    email = models.EmailField()
+    password = models.CharField(max_length=64, validators=[RegexValidator(regex='^.{64}$', message="256-bit expected", code="nomatch")])
     avatar = models.CharField(max_length=128, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True, editable=False, null=False, blank=False)
     points = models.IntegerField(default=0)
     followers = models.IntegerField(default=0)
-
+    
+    flairs = models.ManyToManyField(UserFlair)
     def __str__(self):
         return self.username
 
@@ -37,9 +58,9 @@ class Post(models.Model):
     title = models.CharField(max_length=128)
     content = models.TextField(max_length=32767)
     attached = models.CharField(max_length=128, null=True, blank=True)
-    
+    flair = models.ForeignKey(PostFlair, on_delete=models.SET_NULL, related_name="posts", null=True, blank=True)
+
     views = models.IntegerField(default=0)
-    
     likes = models.IntegerField(default=0)
     dislikes = models.IntegerField(default=0)
     points = models.IntegerField(default=0)
