2e38acce541269be862a5b25c21296e7ec679cef
Andre Holman <andre@andreholman.com>
2022-11-20 13:36:55 -0800

    Add edit and delete features

--- red/models.py
+++ red/models.py
@@ -3,12 +3,12 @@ from unittest.util import _MAX_LENGTH
 
 from django.core.validators import RegexValidator
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 class Sub(models.Model):
     created = models.DateTimeField(auto_now_add=True, editable=False, null=False, blank=False)
-    deleted = models.DateTimeField(auto_now=True, editable=False, null=True, blank=True)
     
     name = models.CharField(max_length=16, validators=[RegexValidator(regex=r"^[0-9a-zA-Z]*$", message="Sub name must be alphanumeric.", code="nomatch")], unique=True)
     followers = models.IntegerField(default=0)
@@ -18,18 +18,26 @@ class Sub(models.Model):
 
 class PostFlair(models.Model): # Can be applied to posts and users.
     created = models.DateTimeField(auto_now_add=True, editable=False, null=False, blank=False)
-    deleted = models.DateTimeField(auto_now=True, editable=False, null=True, blank=True)
+    edited = models.DateTimeField(null=True, blank=True, default=None)
+    deleted = models.DateTimeField(null=True, blank=True, default=None)
 
     text = models.CharField(max_length=64)
     color = models.CharField(max_length=6, validators=[RegexValidator(regex=r"^[a-fA-F0-9]{6}", message='Invalid hex color', code="nomatch")], null=True, blank=True)
-    sub = models.ForeignKey(Sub, on_delete=models.CASCADE, related_name="flairs")
+    sub = models.ForeignKey(Sub, on_delete=models.CASCADE, related_name="postflairs")
 
     def __str__(self):
         return f"{self.text} in {self.sub}"
 
+    def delete(self):
+        self.deleted = timezone.now()
+
+    def edit(self):
+        self.edited = timezone.now()
+
 class UserFlair(models.Model):
     created = models.DateTimeField(auto_now_add=True, editable=False, null=False, blank=False)
-    deleted = models.DateTimeField(auto_now=True, editable=False, null=True, blank=True)
+    edited = models.DateTimeField(null=True, blank=True, default=None)
+    deleted = models.DateTimeField(null=True, blank=True, default=None)
 
     text = models.CharField(max_length=64)
     color = models.CharField(max_length=6, validators=[RegexValidator(regex=r"^[a-fA-F0-9]{6}", message='Invalid hex color', code="nomatch")], null=True, blank=True)
@@ -38,25 +46,41 @@ class UserFlair(models.Model):
     def __str__(self):
         return f"{self.text} in {self.sub}"
 
+    def delete(self):
+        self.deleted = timezone.now()
+
+    def edit(self):
+        self.edited = timezone.now()
+
 class User(models.Model):
     created = models.DateTimeField(auto_now_add=True, editable=False, null=False, blank=False)
-    deleted = models.DateTimeField(auto_now=True, editable=False, null=True, blank=True)
+    edited = models.DateTimeField(null=True, blank=True, default=None)
+    deleted = models.DateTimeField(null=True, blank=True, default=None)
 
     username = models.CharField(max_length=16, unique=True)
     email = models.EmailField()
     password = models.CharField(max_length=64, validators=[RegexValidator(regex='^.{64}$', message="256-bit expected", code="nomatch")])
     avatar = models.CharField(max_length=128, null=True, blank=True)
+    description = models.CharField(max_length=256, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True, editable=False, null=False, blank=False)
     points = models.IntegerField(default=0)
     followers = models.IntegerField(default=0)
     
     flairs = models.ManyToManyField(UserFlair)
+
     def __str__(self):
         return self.username
 
+    def delete(self):
+        self.deleted = timezone.now()
+
+    def edit(self):
+        self.edited = timezone.now()
+
 class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True, editable=False, null=False, blank=False)
-    deleted = models.DateTimeField(auto_now=True, editable=False, null=True, blank=True)
+    edited = models.DateTimeField(null=True, blank=True, default=None)
+    deleted = models.DateTimeField(null=True, blank=True, default=None)
 
     sub = models.ForeignKey(Sub, on_delete=models.CASCADE, related_name="posts")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -73,15 +97,21 @@ class Post(models.Model):
     def __str__(self):
         return self.title + " by " + self.author.username
 
+    def delete(self):
+        self.deleted = timezone.now()
+
+    def edit(self):
+        self.edited = timezone.now()
+
 class Comment(models.Model):
     created = models.DateTimeField(auto_now_add=True, editable=False, null=False, blank=False)
-    deleted = models.DateTimeField(auto_now=True, editable=False, null=True, blank=True)
+    edited = models.DateTimeField(null=True, blank=True, default=None)
+    deleted = models.DateTimeField(null=True, blank=True, default=None)
 
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     parent = models.ForeignKey("self", on_delete=models.CASCADE, related_name="comments", null=True, blank=True)
     content = models.CharField(max_length=1024)
-    edited = models.DateTimeField(auto_now=True, null=True, blank=True)
 
     likes = models.IntegerField(default=0)
     dislikes = models.IntegerField(default=0)
@@ -92,3 +122,9 @@ class Comment(models.Model):
             return str(self.author) + " replies to " + str(self.parent.author) + ": " + self.content
         else:
             return str(self.author) + ": " + self.content
+
+    def delete(self):
+        self.deleted = timezone.now()
+
+    def edit(self):
+        self.edited = timezone.now()
