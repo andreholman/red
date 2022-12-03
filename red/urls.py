1554188f53febfae3a162650df6f07106c1a2db5
Andre Holman <andre@andreholman.com>
2022-12-03 13:02:47 -0800

    Make URLs RESTful

--- red/urls.py
+++ red/urls.py
@@ -5,14 +5,15 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("u/<str:username>/", views.user, name="user"),
-    path("s/<str:sub>/", views.sub, name="sub"),
-    path("s/<str:sub>/p/<int:post_id>/", views.post, name="post"),
-    path("s/<str:sub>/p/<int:post_id>/vote", views.post_vote, name="vote"),
-    path("s/<str:sub>/p/<int:post_id>/update", views.update_post, name="update_post"),
-    path("s/<str:sub>/p/<int:post_id>/delete", views.delete_post, name="delete_post"),
-    path("s/<str:sub>/post", views.post_editor, name="post_editor"),
-    path("s/<str:sub>/create_post", views.create_post, name="create_post"),
-    path("login", views.login, name="login"),
-] + staticfiles_urlpatterns()
+    path("", views.index, name="index"), # GET
+    path("u/<str:username>/", views.user, name="user"), # GET
+    path("s/<str:sub>/", views.sub, name="sub"), # GET
+    path("s/<str:sub>/posts/", views.sub_posts, name="sub_posts"), # GET 303
+    path("s/<str:sub>/posts/<int:post_id>/", views.post, name="post"), # GET
+    path("s/<str:sub>/posts/<int:post_id>/vote/", views.post_vote, name="vote"), # POST
+    path("s/<str:sub>/posts/<int:post_id>/update/", views.update_post, name="update_post"), # GET
+    path("s/<str:sub>/posts/<int:post_id>/delete/", views.delete_post, name="delete_post"), # POST
+    path("s/<str:sub>/post/", views.post_editor, name="post_editor"), # GET
+    path("s/<str:sub>/create_post/", views.create_post, name="create_post"), # POST
+    # path("login", views.login, name="login"), # this will be used later
+] + staticfiles_urlpatterns() # serve statics
