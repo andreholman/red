a9336abe74c2ac7e7e9b954017bda9a642e66f31
Andre Holman <andre@andreholman.com>
2023-05-01 11:05:10 -0700

    Fix static URL for new font

--- back/settings.py
+++ back/settings.py
@@ -5,9 +5,13 @@ env = environ.Env()
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
-if env("ENVIRONMENT_NAME") == "production":
-    print("Running with production settings.")
-    from back.production_settings import *
-elif env("ENVIRONMENT_NAME") == "development":
-    print("Running with development settings.")
-    from back.development_settings import *
+match env("ENVIRONMENT_NAME"):
+    case "production":
+        print("Running with production settings.")
+        from back.production_settings import *
+    case "production_debug":
+        print("Running with debug production settings.")
+        from back.production_debug_settings import *
+    case "development":
+        print("Running with development settings.")
+        from back.development_settings import *
