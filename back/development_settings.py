d45e177a3b07984bb8709e9bab50f7005ab1a792
Andre Holman <andre@andreholman.com>
2023-05-06 18:24:09 -0700

    Revert static collection on local to dev setup

--- back/development_settings.py
+++ back/development_settings.py
@@ -139,6 +139,8 @@ USE_TZ = False
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
